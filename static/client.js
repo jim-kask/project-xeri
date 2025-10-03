fe38e8f132006d65cabc78d7b7fd360ae6460eaf
Dimitrios Kaskouras <dimitris.kaskouras95@gmail.com>
2025-10-03 17:33:57 +0200

    chore: remove unused static\\client.js

--- static/client.js
+++
@@ -1,9 +0,0 @@
-const socket = io();
-
-socket.on('connect', () => {
-  document.getElementById('status').innerText = "Connected!";
-});
-
-socket.on('message', (data) => {
-  console.log("Server says:", data);
-});
